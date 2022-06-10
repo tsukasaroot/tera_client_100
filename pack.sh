1c10047c593ef1dbb39436c606b6aebc3797fd39
Jordan <jordan.rouco-miguez@epitech.eu>
2022-06-10 11:40:18 +0200

    packing into release folder with final dat name

--- pack.sh
+++ pack.sh
@@ -9,28 +9,28 @@ if [ "$1" == '-a' ]; then
 	if [ -f "EUR/unpack/DataCenter.xsd" ]; then
 	        rsync -rtv datasheets/* EUR/unpack/
 		echo "\nCopying system sheets to EUR done\n"
-		./novadrop-dc.exe pack EUR/unpack/ saved-EUR.dat
+		./novadrop-dc.exe pack EUR/unpack/ release/DataCenter_Final_EUR.dat
 	else
 		echo "EUR/unpack/DataCenter.xsd doesn't exist, assuming no DC unpacked for it"
 	fi
         if [ -f "FRA/unpack/DataCenter.xsd" ]; then
                 rsync -rtv datasheets/* FRA/unpack/
                 echo "\nCopying system sheets to FRA done\n"
-		./novadrop-dc.exe pack FRA/unpack/ saved-FRA.dat
+		./novadrop-dc.exe pack FRA/unpack/ release/DataCenter_Final_FRA.dat
         else
                 echo "FRA/unpack/DataCenter.xsd doesn't exist, assuming no DC unpacked for it"
         fi
         if [ -f "GER/unpack/DataCenter.xsd" ]; then
                 rsync -rtv datasheets/* GER/unpack/
                 echo "\nCopying system sheets to GER done\n"
-		./novadrop-dc.exe pack GER/unpack/ saved-GER.dat
+		./novadrop-dc.exe pack GER/unpack/ release/DataCenter_Final_GER.dat
         else
                 echo "GER/unpack/DataCenter.xsd doesn't exist, assuming no DC unpacked for it"
         fi
         if [ -f "RUS/unpack/DataCenter.xsd" ]; then
                 rsync -rtv datasheets/* RUS/unpack/
                 echo "\nCopying system sheets to RUS done\n"
-		./novadrop-dc.exe pack RUS/unpack/ saved-RUS.dat
+		./novadrop-dc.exe pack RUS/unpack/ release/DataCenter_Final_RUS.dat
         else
                 echo "RUS/unpack/DataCenter.xsd doesn't exist, assuming no DC unpacked for it"
         fi
