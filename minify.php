c1af5ade2ac84bba578e74a612d6349662ea59f5
Jordan <jordan.rouco-miguez@epitech.eu>
2022-06-10 18:32:55 +0200

    minify automatically datasheets before packing them (useless tbh)

--- minify.php
+++ minify.php
@@ -1,19 +1,23 @@
 <?php
 ini_set('memory_limit', -1);
-/** @var string $indentedXml */
 
-$indentedXml = file_get_contents('ResourceSummary-00000.xml');
+function getDirContents($path) {
+    $rii = new RecursiveIteratorIterator(new RecursiveDirectoryIterator($path));
 
-$dom = new DOMDocument("1.0");
-
-// Preserve redundant spaces (`true` by default)
-$dom->preserveWhiteSpace = false;
-
-// Disable automatic document indentation
-$dom->formatOutput = false;
-
-$dom->loadXML($indentedXml);
-
-/** @var string $minifiedXml  */
-$minifiedXml = $dom->saveXML();
-file_put_contents('new.xml', $minifiedXml);
+    foreach ($rii as $file)
+        if (!$file->isDir() && strstr($file, 'xml')) {
+	        $fd = $file->getPathname();
+	        $indentedXml = file_get_contents($fd);
+			$dom = new DOMDocument("1.0");
+			// Preserve redundant spaces (`true` by default)
+			$dom->preserveWhiteSpace = false;
+			// Disable automatic document indentation
+			$dom->formatOutput = false;
+			$dom->loadXML($indentedXml, LIBXML_PARSEHUGE);
+			$minifiedXml = $dom->saveXML();
+			file_put_contents($fd, $minifiedXml);
+        }
+}
+echo "\nStarting minification\n";
+getDirContents($argv[1]);
+echo "\nMinification done for {$argv[1]}\n";
--- pack.sh
+++ pack.sh
@@ -1,40 +1,48 @@
 #!/bin/bash
 
 if [ -z "$1" ]; then
-        echo "Args can't be empty, give strings (EUR, FRA, GER, RUS)"
-        exit;
+  echo "Args can't be empty, give strings (EUR, FRA, GER, RUS)"
+  exit
+fi
+
+if [ ! -d "release" ]; then
+  mkdir release
 fi
 
 if [ "$1" == '-a' ]; then
-	if [ -f "EUR/unpack/DataCenter.xsd" ]; then
-	        rsync -rtv datasheets/* EUR/unpack/
-		echo "\nCopying system sheets to EUR done\n"
-		./novadrop-dc.exe pack EUR/unpack/ release/DataCenter_Final_EUR.dat
-	else
-		echo "EUR/unpack/DataCenter.xsd doesn't exist, assuming no DC unpacked for it"
-	fi
-        if [ -f "FRA/unpack/DataCenter.xsd" ]; then
-                rsync -rtv datasheets/* FRA/unpack/
-                echo "\nCopying system sheets to FRA done\n"
-		./novadrop-dc.exe pack FRA/unpack/ release/DataCenter_Final_FRA.dat
-        else
-                echo "FRA/unpack/DataCenter.xsd doesn't exist, assuming no DC unpacked for it"
-        fi
-        if [ -f "GER/unpack/DataCenter.xsd" ]; then
-                rsync -rtv datasheets/* GER/unpack/
-                echo "\nCopying system sheets to GER done\n"
-		./novadrop-dc.exe pack GER/unpack/ release/DataCenter_Final_GER.dat
-        else
-                echo "GER/unpack/DataCenter.xsd doesn't exist, assuming no DC unpacked for it"
-        fi
-        if [ -f "RUS/unpack/DataCenter.xsd" ]; then
-                rsync -rtv datasheets/* RUS/unpack/
-                echo "\nCopying system sheets to RUS done\n"
-		./novadrop-dc.exe pack RUS/unpack/ release/DataCenter_Final_RUS.dat
-        else
-                echo "RUS/unpack/DataCenter.xsd doesn't exist, assuming no DC unpacked for it"
-        fi
+  if [ -f "EUR/unpack/DataCenter.xsd" ]; then
+    rsync -rtv datasheets/* EUR/unpack/
+    echo -e "\nCopying system sheets to EUR done\n"
+    php minify.php EUR/unpack
+    ./novadrop-dc.exe pack EUR/unpack/ release/DataCenter_Final_EUR.dat
+  else
+    echo -e "\nEUR/unpack/DataCenter.xsd doesn't exist, assuming no DC unpacked for it"
+  fi
+  if [ -f "FRA/unpack/DataCenter.xsd" ]; then
+    rsync -rtv datasheets/* FRA/unpack/
+    echo -e "\nCopying system sheets to FRA done\n"
+    php minify.php FRA/unpack
+    ./novadrop-dc.exe pack FRA/unpack/ release/DataCenter_Final_FRA.dat
+  else
+    echo -e "\nFRA/unpack/DataCenter.xsd doesn't exist, assuming no DC unpacked for it"
+  fi
+  if [ -f "GER/unpack/DataCenter.xsd" ]; then
+    rsync -rtv datasheets/* GER/unpack/
+    echo -e "\nCopying system sheets to GER done\n"
+    php minify.php GER/unpack
+    ./novadrop-dc.exe pack GER/unpack/ release/DataCenter_Final_GER.dat
+  else
+    echo -e "\nGER/unpack/DataCenter.xsd doesn't exist, assuming no DC unpacked for it"
+  fi
+  if [ -f "RUS/unpack/DataCenter.xsd" ]; then
+    rsync -rtv datasheets/* RUS/unpack/
+    echo -e "\nCopying system sheets to RUS done\n"
+    php minify.php RUS/unpack
+    ./novadrop-dc.exe pack RUS/unpack/ release/DataCenter_Final_RUS.dat
+  else
+    echo -e "\nRUS/unpack/DataCenter.xsd doesn't exist, assuming no DC unpacked for it"
+  fi
 else
-        rsync -rtv datasheets/* "$1"/unpack/
-        ./novadrop-dc.exe pack "$1"/unpack/ saved-"$1".dat
+  rsync -rtv datasheets/* "$1"/unpack/
+  ./novadrop-dc.exe pack "$1"/unpack/ saved-"$1".dat
 fi
